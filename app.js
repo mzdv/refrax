db0979cf21a5caf11f0d780e3625dc422c1f0e1e
Milos Zivadinovic <mzdv@outlook.com>
2015-11-19 18:59:36 +0100

    Setting up a stream scaffolding.

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 var http = require('http');
-var stream = require('stream').Writable;
+var Duplex = require('stream').Duplex;
+var mutator = new Duplex();
+
+var Writable = require('stream').Duplex;
+var logs = new Duplex();
+
+var test;
+var test2;
+
+mutator._read = function() {
+	console.log("Mutator exit");
+	mutator.push(test);
+	mutator.push(null);
+}
+
+mutator._write = function(chunk, enc, next) {
+	console.log("Mutator entry: " + chunk);
+	test = chunk;
+	next();
+}
+
+logs._write = function(chunk, enc, next) {
+	console.log("Rhythm is a dancer: " + chunk);
+	test2 = chunk;
+	next();
+}
+
+logs._read = function() {
+	logs.push(test2);
+	logs.push(null);
+}
+// var loggingData = new Readable;
 
 var httpServer = http.createServer(function (req, res) {
-	var headers = req.headers;
+	req.pipe(mutator).pipe(logs).pipe(res);
+	// res.end();
+		
+	// var headers = req.headers;
 
-	console.log(new Date());
+	// console.log(new Date());
 
-	for (key in headers) {
-		console.log(key + ': ' + headers[key] + '\n');
-	}
+	// for (key in headers) {
+	// 	console.log(key + ': ' + headers[key] + '\n');
+	// }
 
-	console.log('url: ' + req.url + '\n');
-	console.log('verb: ' + req.method + '\n');
+	// console.log('url: ' + req.url + '\n');
+	// console.log('verb: ' + req.method + '\n');
 
-	res.writeHead('200');
-	res.end('<script>window.location="' + req.url + '";</script>');		// TO-DO: do not perform endless redirects
-}
-	)
+	// res.writeHead('200');
+	// res.end('<script>window.location="' + req.url + '";</script>');		// TO-DO: do not perform endless redirects
+})
 	.listen(8080, '127.0.0.1');
